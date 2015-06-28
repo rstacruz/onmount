0610d58a1ae8b67bff5ce73c2c29dd3c0e271bdd
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-06-28 18:28:47 +0800

    Update IIFE prefix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* global define */
-;(function (root, factory) {
+void (function (root, factory) {
   if (typeof define === 'function' && define.amd) define(factory)
   else if (typeof exports === 'object') module.exports = factory()
   else {
