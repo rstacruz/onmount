a2cb5f9978dfd58c9196bc8f67d6616a565c1d0b
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-07-04 12:47:32 +0800

    Make tests zuul-compatible

--- test/setup.js
+++ test/setup.js
@@ -3,13 +3,11 @@
 if (typeof process === 'object') {
   module.exports = function () {
     var jsdom = require('mocha-jsdom')
-    var rerequire = jsdom.rerequire
-
     jsdom()
 
     before(function () {
       global.expect = require('expect')
-      global.onmount = rerequire('../index')
+      global.onmount = require('../index')
     })
   }
 } else {
