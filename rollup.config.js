90eaf4e80be282d19289252e04003b558fde0cce
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-03-05 18:21:07 +0800

    Export an es build

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,17 @@ import buble from 'rollup-plugin-buble'
 
 export default {
   input: 'index.js',
-  output: {
-    file: 'dist/onmount.js',
-    format: 'umd',
-    name: 'onmount'
-  },
+  output: [
+    {
+      file: 'dist/onmount.js',
+      format: 'umd',
+      name: 'onmount'
+    },
+    {
+      file: 'dist/onmount.es.js',
+      format: 'es'
+    }
+  ],
   plugins: [
     buble()
   ]
