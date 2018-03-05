0c96cbfdb1655ab54dd8a030dfce69bb3b653417
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-03-05 16:31:12 +0800

    Update detect_mutate.test.js

--- test/index.js
+++ test/index.js
@@ -1,11 +1,3 @@
 require('jsdom-global')()
-
-require('tape')('dom', function (t) {
-  t.pass(navigator.userAgent)
-  t.end()
-})
-
 require('./with_role_test')
 require('./state_test')
-require('./detect_mutate_test')
-require('tape')('eslint', require('tape-eslint')())
