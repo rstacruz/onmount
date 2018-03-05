999809e73512671de95e865ab3b92684ea2c1e53
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-03-05 17:27:08 +0800

    Update state.test.js

--- test/index.js
+++ test/index.js
@@ -1,3 +1,2 @@
 require('jsdom-global')()
 require('./with_role_test')
-require('./state_test')
---
+++ tests/state.test.js
@@ -0,0 +1,40 @@
+var onmount = require('../index')
+var { el, remove } = require('../lib/test_helpers')
+
+describe('state', () => {
+  let div, state
+
+  beforeEach(() => {
+    onmount('.our-behavior', function (b) {
+      state = b
+      b.number = 10
+    }, function (b) {
+      b.number += 1
+    })
+
+    // Trigger the on-mount handler
+    div = el('div', { 'class': 'our-behavior' })
+    onmount()
+
+    // Trigger the on-unmount handler
+    remove(div)
+    onmount()
+  })
+
+  afterEach(() => {
+    remove(div)
+    onmount.reset()
+  })
+
+  it('has an id', () => {
+    expect(state['id']).toMatch(/^c\d+$/)
+  })
+
+  it('passes the selector', () => {
+    expect(state.selector).toEqual('.our-behavior')
+  })
+
+  it('works', () => {
+    expect(state.number).toEqual(11)
+  })
+})
