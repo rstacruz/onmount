bdc7f8a8796c19d077b836aea186a7cc008e7884
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-03-06 15:17:26 +0800

    Initial attempt to refactor Behavior

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { each, isjQuery, isEvent, has, query, isAttached } from './lib/helpers'
+import Behavior from './lib/behavior'
 
 /*
  * Internal: IDs for auto-incrementing.
@@ -50,7 +51,7 @@ function onmount (selector, init, exit, options) {
     // onmount(sel, fn, [fn]) - register a new behavior.
     var be = new Behavior(selector, init, exit, options)
     onmount.behaviors.push(be)
-    be.register()
+    be.register(register)
   }
 
   return this
@@ -144,85 +145,6 @@ onmount.reset = function reset () {
   onmount.handlers = []
   onmount.selectors = {}
   onmount.behaviors = []
-}
-
-/**
- * Internal: behavior class
- */
-
-function Behavior (selector, init, exit, options) {
-  this.id = 'b' + bid++
-  this.init = init
-  this.exit = exit
-  this.selector = selector
-  this.loaded = [] // keep track of dom elements loaded for this behavior
-  this.key = '__onmount:' + bid // leave the state in el['__onmount:12']
-  this.detectMutate = options && options.detectMutate
-}
-
-/**
- * Internal: initialize this behavior by registering itself to the internal
- * `selectors` map. This allows you to call `onmount(selector)` later on.
- */
-
-Behavior.prototype.register = function () {
-  const { loaded, selector } = this
-
-  register(selector, () => {
-    const list = query(selector)
-
-    // This is the function invoked on `onmount(selector)`.
-    // Clean up old ones (if they're not in the DOM anymore).
-    each(loaded, (element, i) => {
-      this.visitExit(element, i, list)
-    })
-
-    // Clean up new ones (if they're not loaded yet).
-    each(list, (element) => {
-      this.visitEnter(element)
-    })
-  })
-}
-
-/**
- * Internal: visits the element `el` and turns it on if applicable.
- */
-
-Behavior.prototype.visitEnter = function (el) {
-  if (el[this.key]) return
-  var options = { id: 'c' + cid, selector: this.selector }
-  if (this.init.call(el, options) !== false) {
-    el[this.key] = options
-    this.loaded.push(el)
-    cid++
-  }
-}
-
-/**
- * Internal: visits the element `el` and sees if it needs its exit handler
- * called.
- */
-
-Behavior.prototype.visitExit = function (el, i, list) {
-  if (!el) return
-  if (this.detectMutate) {
-    if (!has(list, el)) return this.doExit(el, i)
-  } else {
-    if (!isAttached(el)) return this.doExit(el, i)
-  }
-}
-
-/**
- * Internal: calls the exit handler for the behavior for element `el` (if
- * available), and marks the behavior/element as uninitialized.
- */
-
-Behavior.prototype.doExit = function (el, i) {
-  if (typeof i === 'undefined') i = this.loaded.indexOf(el)
-  this.loaded[i] = undefined
-  if (this.exit && this.exit.call(el, el[this.key]) !== false) {
-    delete el[this.key]
-  }
 }
 
 /**
